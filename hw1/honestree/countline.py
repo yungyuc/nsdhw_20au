4b9432520370561bee31edf9f7fb5e45a0017af0
Hao-Hsiang Kuo <13-kuohh@users.noreply.git.cs.nctu.edu.tw>
2020-09-29 13:59:08 +0800

    merge countline.py realcountline.py with EOF

--- hw1/honestree/countline.py
+++ hw1/honestree/countline.py
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-eval $PYTHON_BIN realcountline.py $1
+eval $PYTHON_BIN << EOL
+import sys
+import os.path
+
+fname = "$1"
+if os.path.exists(fname):
+    with open(fname) as fobj:
+        lines = fobj.readlines()
+    sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+else:
+    sys.stdout.write('{} not found\n'.format(fname))
+EOL
+
+if [ $? -ne 0 ]
+  then
+  echo "exec: $PYTHON_BIN: not found"
+    exit 1
+  else
+    exit 0
+fi
+
