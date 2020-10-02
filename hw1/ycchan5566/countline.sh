aded81079933e227edd900f8e3231d689b12c4b9
ycchan5566 <josh0408tw@gmail.com>
2020-10-02 11:15:59 +0000

    Finish hw1

---
+++ hw1/ycchan5566/countline.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$#" -lt 1 ]; then
+	echo "missing file name"
+elif [ "$#" -gt 1 ]; then 
+	echo "only one argument is allowed"
+elif [ -f "$1" ]; then
+	echo $(wc $1 | awk '{print $1}') "lines in" "$1"
+else
+	echo "$1" "not found"
+fi
