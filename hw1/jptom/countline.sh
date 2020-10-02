35e6694caa7076eb3872a1ef08cab92512451338
jptom <fmv.asdjk0407.lifebook@gmail.com>
2020-10-02 19:28:53 +0800

    update countline.sh

--- hw1/jptom/countline.sh
+++ hw1/jptom/countline.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-result=$(wc -l $1 | tr -dc '0-9')
-echo "$result lines in  $1"
+
+if [ "$#" -lt 1 ]; then
+  echo "missing file name"
+elif [ "$#" -gt 1 ]; then
+  echo "onely one argument is allowed"
+else
+  if [ -f $1 ]; then
+    result=$(wc -l $1 | tr -dc '0-9')
+    echo "$result lines in  $1"
+  else
+    echo "$1 not found"
+  fi
+fi
+
