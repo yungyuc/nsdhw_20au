068a128f78052f527931427748a251b26852ea0e
jptom <fmv.asdjk0407.lifebook@gmail.com>
2020-10-07 20:21:12 +0800

    modified hw2 all file

--- hw2/jptom/q2/test_q2.py
+++ hw2/jptom/q2/test_q2.py
@@ -1,5 +1,48 @@
 #!/usr/bin/env python
+import _vector
+import numpy as np
+import numpy.testing as npt
 
-def test_q2():
-    True
+def transform_2d(vec, theta):
+    t_mat = np.array([[np.cos(theta),   np.sin(theta)],
+                      [-np.sin(theta), np.cos(theta)]])
+    return np.dot(t_mat, vec)
+
+def test_zero_vector():
+    vec = np.random.randn(2)
+    zero_vec = np.zeros(2)
+    npt.assert_raises(ValueError, _vector.cal_2d_angle, vec, zero_vec)
+    npt.assert_raises(ValueError, _vector.cal_2d_angle, zero_vec, vec)
+    npt.assert_raises(ValueError, _vector.cal_2d_angle, zero_vec, zero_vec)
+
+def test_angle_0():
+    vec1 = np.random.randn(2)
+    vec2 = vec1
+    angle = _vector.cal_2d_angle(vec1, vec2)
+    npt.assert_almost_equal(angle, 0/180*np.pi)
+
+def test_angle_90():
+    vec1 = np.random.randn(2)
+    vec2 = transform_2d(vec1, 90/180*np.pi)
+    angle = _vector.cal_2d_angle(vec1, vec2)
+    npt.assert_almost_equal(angle, 90/180*np.pi)
+    angle = _vector.cal_2d_angle(vec2, vec1)
+    npt.assert_almost_equal(angle, -90/180*np.pi)
+
+def test_angle_180():
+    vec1 = np.random.randn(2)
+    vec2 = transform_2d(vec1, 180/180*np.pi)
+    angle = _vector.cal_2d_angle(vec1, vec2)
+    npt.assert_almost_equal(angle, 180/180*np.pi)
+    angle = _vector.cal_2d_angle(vec2, vec1)
+    npt.assert_almost_equal(angle, -180/180*np.pi)
+
+def test_angle_random():
+    vec1 = np.random.randn(2)
+    random_angle = np.random.rand()*180
+    vec2 = transform_2d(vec1, random_angle/180*np.pi)
+    angle = _vector.cal_2d_angle(vec1, vec2)
+    npt.assert_almost_equal(angle, random_angle/180*np.pi)
+    angle = _vector.cal_2d_angle(vec2, vec1)
+    npt.assert_almost_equal(angle, -random_angle/180*np.pi)
 
--- hw2/jptom/q2/vector.cpp
+++ hw2/jptom/q2/vector.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
-#include <vector>
 #include <cmath>
 
 namespace py = pybind11;
 
-template <typename T1, typename T2>
-py::array_t<double> cal_2d_angle(py::array_t<T1> vec1, py::array_t<T2> vec2)
+py::array_t<double> cal_2d_angle(py::array_t<double> vec1, py::array_t<double> vec2)
 {
-	const py::buffer_info & vec1_info = vec1.request();
-	const py::buffer_info & vec2_info = vec2.request();
+	py::buffer_info vec1_info = vec1.request();
+	py::buffer_info vec2_info = vec2.request();
 
-	const ssize_t & vec1_ndim = vec1_info.ndim;
-	const ssize_t & vec2_ndim = vec2_info.ndim;
-	if ( vec1_ndim != 1 || vec2_ndim != 1 ) { throw std::invalid_argument("invalid input. input vector ndidm must be 1."); }
+	if ( vec1_info.ndim != 1 || vec2_info.ndim != 1 ) { throw std::invalid_argument("invalid input. input vector ndidm must be 1."); }
 
-	const auto &vec1_shape = vec1_info.shape;
-	const auto &vec2_shape = vec2_info.shape;
-	if ( vec1_shape[0] != 2 || vec2_shape[0] != 2 ) { throw std::invalid_argument("invalid input. input vector shape must be (2,)."); }
+	if ( vec1_info.size != 2 || vec2_info.size != 2 ) { throw std::invalid_argument("invalid input. input vector size must be 2."); }
 
 	double * data1 = (double *) vec1_info.ptr;
 	double * data2 = (double *) vec2_info.ptr;
 	if ( (data1[0]==0 && data1[1]==0) || (data2[0]==0 && data2[1]==0) ) { throw std::invalid_argument("invalid input. input vector has zero length."); }
-
+	//
+	//std::cout<<"vec1:[ "<<data1[0]<<", "<<data1[1]<<"]"<<std::endl;
+	//std::cout<<"vec2:[ "<<data2[0]<<", "<<data2[1]<<"]"<<std::endl;
+	//
+	//std::cout<<"vec1:[ "<<*vec1.data(0)<<", "<<*vec1.data(1)<<"]"<<std::endl;
+	//std::cout<<"vec2:[ "<<*vec2.data(0)<<", "<<*vec2.data(1)<<"]"<<std::endl;
+	//
 	double dot = data1[0]*data2[0] + data1[1]*data2[1];
-	double cross = data1[0]*data2[1] - data1[1]*data2[0];
+	double cross = data1[1]*data2[0] - data1[0]*data2[1];
 	double angle = atan2(cross, dot);
-	std::cout<<"c++ result: "<<angle<<std::endl;
+	//
+	//std::cout<<"c++ result: "<<angle<<std::endl;
+	//
 	py::array_t<double> result({1});
 	*result.mutable_data(0) = angle;
 	return result;
@@ -36,8 +38,5 @@ py::array_t<double> cal_2d_angle(py::array_t<T1> vec1, py::array_t<T2> vec2)
 PYBIND11_MODULE(_vector, m)
 {
 	m.doc() = "hw2 q2 module";
-	m.def("cal_2d_angle", &cal_2d_angle<int32_t,int32_t>, "calculate the angle between two vectors");
-	m.def("cal_2d_angle", &cal_2d_angle<int32_t,double>, "calculate the angle between two vectors");
-	m.def("cal_2d_angle", &cal_2d_angle<double,int32_t>, "calculate the angle between two vectors");
-	m.def("cal_2d_angle", &cal_2d_angle<double,double>, "calculate the angle between two vectors");
+	m.def("cal_2d_angle", &cal_2d_angle, "calculate the angle between two vectors");
 }
