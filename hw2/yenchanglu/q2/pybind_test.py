59112f1ea1d9e1243c1f40126209dad2473506cf
leonlu <yenchanglu@gmail.com>
2020-10-09 23:12:07 +0800

    fix AssertionErrors

--- hw2/yenchanglu/q2/pybind_test.py
+++ hw2/yenchanglu/q2/pybind_test.py
@@ -8,14 +8,12 @@ def testInvalidVector():
         vec2d.getAngle([1],[1,2])
 
 def testZeroLen():
-    get_angle = vec2d.getAngle([0,0], [0,1])
-    assert math.isclose(get_angle, 0, rel_tol=1e-06)
-    get_angle2 = vec2d.getAngle([0,0], [1,0])
-    assert math.isclose(get_angle2, 0, rel_tol=1e-06)
+    get_angle = vec2d.getAngle([0,0], [0,0])
+    assert math.isclose(get_angle, -1, rel_tol=1e-06)
 
 def test45Deg():
     get_angle = vec2d.getAngle([1,1], [0,1])
-    assert math.isclose(get_angle, math.pi/4, rel_tol=1e-06)
+    assert math.isclose(get_angle, math.radians(45), rel_tol=1e-06)
 
 def testRightAngle():
     get_angle = vec2d.getAngle([1,0], [0,1])
