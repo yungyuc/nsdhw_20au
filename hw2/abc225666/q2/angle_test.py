b23960929ac9ec6ad14419acf175faacc0944e4e
Ting-Rui, Wang <qwe50120@gmail.com>
2020-10-10 06:14:44 +0000

    chore: Modify test case

--- hw2/abc225666/q2/angle_test.py
+++ hw2/abc225666/q2/angle_test.py
@@ -2,16 +2,21 @@ import _vector
 import math
 import pytest
 
-def test_45():
+def test_zero_length():
+    assert math.isnan(_vector.angle(0, 0, 0, 0))
+
+def test_0_angle():
+    assert math.isclose(_vector.angle(300, 600, 15, 30), 0)
+
+def test_right_angle():
+    assert math.isclose(_vector.angle(1000, 1000, -2000, 2000), math.pi/2)
+
+def test_45_angle():
     # 45 degree
     assert math.isclose(_vector.angle(0, 100, 5, 5),  math.pi/4)
 
-def test_30():
+def test_30_angle():
     # 30 degree
     assert math.isclose(_vector.angle(math.sqrt(3), 1, 1, math.sqrt(3)), math.pi/6)
 
-def test_0_vector():
-    assert math.isnan(_vector.angle(0, 0, 1, 200))
 
-def test_90():
-    assert math.isclose(_vector.angle(1000, 1000, -2000, 2000), math.pi/2)
