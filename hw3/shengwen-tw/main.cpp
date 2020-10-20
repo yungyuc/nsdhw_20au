281927b4db561efdaed78fe81d3de01a3b7ed171
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-10-20 21:40:53 +0800

    arrange answer output

--- hw3/shengwen-tw/main.cpp
+++ hw3/shengwen-tw/main.cpp
@@ -126,20 +126,36 @@ int main(void)
 	start_time = time();
 	multiply_naive(mat1, mat2, m_naive);
 	end_time = time();
-	cout << "time cost of the naive multiply: " << end_time - start_time << " seconds\n";
+	double cost_naive = end_time - start_time;
+	printf("time cost of the naive multiply: %lf seconds\n", cost_naive);
 
 	start_time = time();
 	multiply_tile(mat1, mat2, m_tile);
 	end_time = time();
-	cout << "time cost of the tiled multiply: " << end_time - start_time << " seconds\n";
+	double cost_tile = end_time - start_time;
+	printf("time cost of the tile multiply: %lf seconds\n", cost_tile);
 
 	start_time = time();
 	multiply_mkl(mat1, mat2, m_mkl);
 	end_time = time();
-	cout << "time cost of the mkl: " << end_time - start_time << " seconds\n";
+	double cost_mkl = end_time - start_time;;
+	printf("time cost of the mkl multiply: %lf seconds\n", cost_mkl);
 
-	printf("difference between m_naive and m_tile is %d\n", matrix_compare(m_naive, m_tile));
-	printf("difference between m_tile and m_mkl is %d\n", matrix_compare(m_tile, m_mkl));
+	printf("tile multiply is %lfx times faster than naive multiply\n", cost_naive / cost_tile);
+
+	printf("\nmatrix correctness checking:\n");
+	int naive_tile_diff = matrix_compare(m_naive, m_tile);
+	int tile_mkl_diff = matrix_compare(m_tile, m_mkl);
+	int naive_mkl_diff = matrix_compare(m_naive, m_mkl);
+	printf("%d elements of m_naive and m_tile are differenct\n", naive_tile_diff);
+	printf("%d elements of m_tile and m_mkl are different\n", tile_mkl_diff);
+	printf("%d elements of m_naive and m_mkl are differenct\n", naive_mkl_diff);
+
+	if((naive_tile_diff == 0) && (tile_mkl_diff == 0) && (naive_mkl_diff == 0)) {
+		printf("-> correct, all answer are matched!\n");
+	} else {
+		printf("-> failed, something is wrong!\n");
+	}
 
 #if 0
 	print_matrix("mat1", mat1);
