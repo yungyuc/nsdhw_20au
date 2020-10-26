ad6441529c85e5b8ae84160e98892aa4f276bc15
leonlu <yenchanglu@gmail.com>
2020-10-27 03:09:13 +0800

    fix typo & unused variable

--- hw3/yenchanglu/_matrix.cpp
+++ hw3/yenchanglu/_matrix.cpp
@@ -137,12 +137,11 @@ Matrix multiply_tile(Matrix const &mat1, Matrix const &mat2, size_t tile_size) {
 
     const size_t nrow1 = mat1.nrow();
     const size_t ncol1 = mat1.ncol();
-    const size_t nrow2 = mat2.nrow();
     const size_t ncol2 = mat2.ncol();
 
-    for (size_t i = 0; i < nrow1(); i += tile_size) {
-        for (size_t k = 0; k < ncol2(); k += tile_size) {
-            for (size_t j = 0; j < nrow1(); j += tile_size) {
+    for (size_t i = 0; i < nrow1; i += tile_size) {
+        for (size_t k = 0; k < ncol2; k += tile_size) {
+            for (size_t j = 0; j < nrow1; j += tile_size) {
                 for (size_t tile_j = j; tile_j < ((tile_size + j) > ncol1 ? ncol1 : (tile_size + j)); ++tile_j) {
                     for (size_t tile_i = i; tile_i < ((tile_size + i) > nrow1 ? nrow1 : (tile_size + i)); ++tile_i) {
                         for (size_t tile_k = k; tile_k < ((tile_size + k) > ncol2 ? ncol2 : (tile_size + k)); ++tile_k) {
