dc0c4ec17004a1a9edfd95aa43e46c8624f8e377
leonlu <yenchanglu@gmail.com>
2020-10-26 23:30:03 +0800

    for test

--- hw3/yenchanglu/test_matrix.py
+++ hw3/yenchanglu/test_matrix.py
@@ -2,40 +2,15 @@ import pytest
 import numpy as np
 import math
 import _matrix
+import timerit
 
-def exec_time():
+def test_naive():
     m1 = np.random.random((1151, 1151))
     m2 = np.random.random((1151, 1151))
 
     mat1 = _matrix.Matrix(m1)
     mat2 = _matrix.Matrix(m2)
 
-    mat_ret = _matrix.multiply_naive(mat1, mat2)
+    #mat_ret = _matrix.multiply_naive(mat1, mat2)
 
-    assert np.array(mat_ret) == pytest.approx(np.matmul(m1,m2))
-
-    '''timer_naive = []
-    timer_tile = []
-    timer_mkl = []
-
-    for i in range(5):
-        timer = time.time()
-        mat_naive = _matrix.multiply_naive(mat1, mat2)
-        timer_naive.append(time.time() - timer)
-
-        timer = time.time()
-        mat_tile = _matrix.multiply_tile(mat1, mat2, 64)
-        timer_tile.append(time.time() - timer)
-
-        timer = time.time()
-        mat_mkl = _matrix.multiply_mkl(mat1, mat2)
-        timer_mkl.append(time.time() - timer)
-
-    with open("performance.txt", "w") as f:
-        f.write("Average execution time\n")
-        f.write("mutiply_naive: {} secs\n".format("%.4f"%np.average(timer_naive)))
-        f.write("multiply_tile: {} secs\n".format("%.4f"%np.average(timer_tile)))
-        f.write("multiply_mkl: {} secs\n".formate("%.4f"%np.average(timer_mkl)))
-        f.write("The tiling version is {:.1%} faster than naive version\n".format(timer_naive / timer_tile))
-        f.write("The mkl version is {:.1%} faster than naive version\n".format(timer_naive / timer_mkl))
-        f.close()'''
+    assert np.matmul(m1,m2) == pytest.approx(np.matmul(m1,m2))
