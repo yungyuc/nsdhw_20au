39b59f571142a885b9a9784ec97e8137ceb4d351
leonlu <yenchanglu@gmail.com>
2020-10-26 23:45:31 +0800

    fix unittest type error

--- hw3/yenchanglu/test_matrix.py
+++ hw3/yenchanglu/test_matrix.py
@@ -1,15 +1,19 @@
 import pytest
 import numpy as np
 import math
-import _matrix
+import _matrix import Matrix, multiply_naive, multiply_tile, multiply_mkl
 
 def test_naive():
-    m1 = np.random.random((1151, 1151))
-    m2 = np.random.random((1151, 1151))
+    m = np.random.randint(600, 1151)
+    n = np.random.randint(600, 1151)
+    k = np.random.randint(600, 1151)
 
-    mat1 = _matrix.Matrix(m1)
-    mat2 = _matrix.Matrix(m2)
+    np_mat1 = np.random.random((m, k))
+    np_mat2 = np.random.random((k, n))
 
-    mat_ret = _matrix.multiply_naive(mat1, mat2)
+    mat1 = Matrix(np_mat1)
+    mat2 = Matrix(np_mat2)
 
-    assert np.array(mat_ret) == pytest.approx(np.matmul(m1,m2))
+    mat_ret = multiply_naive(mat1, mat2)
+
+    assert np.array(mat_ret) == pytest.approx(np.matmul(np_mat1,np_mat2))
