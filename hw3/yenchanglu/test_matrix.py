536b8931ef903bed5c0febf646b7346ac05b5d21
leonlu <yenchanglu@gmail.com>
2020-10-26 23:34:49 +0800

    fix import module error

--- hw3/yenchanglu/test_matrix.py
+++ hw3/yenchanglu/test_matrix.py
@@ -2,7 +2,6 @@ import pytest
 import numpy as np
 import math
 import _matrix
-import timerit
 
 def test_naive():
     m1 = np.random.random((1151, 1151))
@@ -11,6 +10,6 @@ def test_naive():
     mat1 = _matrix.Matrix(m1)
     mat2 = _matrix.Matrix(m2)
 
-    #mat_ret = _matrix.multiply_naive(mat1, mat2)
+    mat_ret = _matrix.multiply_naive(mat1, mat2)
 
-    assert np.matmul(m1,m2) == pytest.approx(np.matmul(m1,m2))
+    assert np.array(mat_ret) == pytest.approx(np.matmul(m1,m2))
