3d102389236644d41c7ac980a41496225e05e8ed
林宏文(Tommy Lin) <gnat_lin@hotmail.com>
2020-11-23 20:53:13 +0800

    Clean up include header files

--- hw5/TommyLin/matrix.cpp
+++ hw5/TommyLin/matrix.cpp
@@ -1,11 +1,7 @@
-#include <iostream>
-#include <pybind11/numpy.h>
-#include <pybind11/operators.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include <vector>
+#include <stdexcept>
 #include "mkl.h"
 
-namespace py = pybind11;
 
 class Matrix {
 
