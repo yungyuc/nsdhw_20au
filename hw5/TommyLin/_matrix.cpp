a466dadb3aaad1ee55da16a595b0cc268025c05a
林宏文(Tommy Lin) <tommy.hw.lin@foxconn.com>
2020-11-23 17:38:56 +0800

    Introduce matrix.hpp
    Makefile changes:
    1. Ignore -Werror
    2. Extract MKLLINKLINE from CXXFLAGS to LDFLAGS
    3. Hide rm command and show files removed

--- hw5/TommyLin/_matrix.cpp
+++ hw5/TommyLin/_matrix.cpp
@@ -149,31 +149,3 @@ Matrix multiply_mkl(const Matrix &mat1, const Matrix &mat2)
     return ret;
 };
 
-PYBIND11_MODULE(_matrix, m) {
-    m.def("multiply_naive", &multiply_naive);
-    m.def("multiply_tile", &multiply_tile);
-    m.def("multiply_mkl", &multiply_mkl);
-    py::class_<Matrix>(m, "Matrix", py::buffer_protocol())
-        .def(py::init<size_t, size_t>())
-        .def(py::init<Matrix const &>())
-        .def(py::init<std::vector<std::vector<double>>&>())
-        .def_property("nrow", &Matrix::nrow, nullptr)
-        .def_property("ncol", &Matrix::ncol, nullptr)
-        .def_buffer([] (Matrix &m) -> py::buffer_info {
-            return py::buffer_info(
-                m.data(),
-                sizeof(double),
-                py::format_descriptor<double>::format(),
-                2,
-                { m.nrow(), m.ncol() },
-                { sizeof(double) * m.ncol(), sizeof(double) }
-            );
-        })
-        .def("__eq__", &Matrix::operator==)
-        .def("__setitem__", [](Matrix &mat1, std::pair<size_t, size_t> i, float v) {
-            mat1(i.first, i.second) = v;
-        })
-        .def("__getitem__", [](Matrix &mat1, std::pair<size_t, size_t> i) {
-            return mat1(i.first, i.second);
-        });
-}
