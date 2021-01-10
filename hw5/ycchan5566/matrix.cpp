490a86ef65a561fbc4ca284026c872c4ab68eadd
ycchan5566 <josh0408tw@gmail.com>
2021-01-10 23:57:41 +0800

    Complete hw5

---
+++ hw5/ycchan5566/matrix.cpp
@@ -0,0 +1,67 @@
+#include <mkl.h>
+#include "matrix.hpp"
+
+Matrix multiply_naive(const Matrix &mat1, const Matrix &mat2) {
+    size_t nr = mat1.nrow();
+    size_t nc = mat2.ncol();
+    Matrix ans(nr, nc);
+    for (size_t i = 0; i<nr; i++) {
+        for (size_t k = 0; k < nc; k++) {
+            double sum = 0;
+            for (size_t j = 0; j < mat1.ncol(); j++) {
+                sum += mat1(i, j) * mat2(j, k);
+            }
+            ans(i, k) = sum;
+        }
+    }
+    return ans;
+}
+
+Matrix multiply_tile(Matrix &mat1, Matrix &mat2, int tile_size) {
+  Matrix mat3(mat1.nrow(), mat2.ncol());
+
+  for (int i = 0; i < mat1.nrow(); i += tile_size) {
+    for (int j = 0; j < mat2.ncol(); j += tile_size) {
+
+      int start = mat1.nrow() < i + tile_size ? mat1.nrow() : i + tile_size;
+      int end = mat2.ncol() < j + tile_size ? mat2.ncol() : j + tile_size;
+
+      for (int k = 0; k < mat1.ncol(); k += tile_size) {
+        int ret_edge = mat1.ncol() < k + tile_size ? mat1.ncol() : k + tile_size;
+        for (int kk = k; kk < ret_edge; kk++) {
+          for (int ii = i; ii < start; ii++) {
+            for (int jj = j; jj < end; jj++) {
+              mat3(ii, jj) += mat1(ii, kk) * mat2(kk, jj);
+            }
+          }
+        }
+      }
+    }
+  }
+  return mat3;
+}
+
+Matrix * multiply_mkl(const Matrix & mat1, const Matrix & mat2)
+{
+	mkl_set_num_threads(1);
+
+	Matrix * ret = new Matrix(mat1.nrow(), mat2.ncol());
+
+	cblas_dgemm(
+		CblasRowMajor,
+		CblasNoTrans,
+		CblasNoTrans,
+		mat1.nrow(),
+		mat2.ncol(),
+		mat1.ncol(),
+		1.0,
+		mat1.m_buffer.data(),
+		mat1.ncol(),
+		mat2.m_buffer.data(),
+		mat2.ncol(),
+		0.0,
+		(*ret).m_buffer.data(),
+		(*ret).ncol()
+		);
+	return ret;
+}
---
+++ hw5/ycchan5566/matrix.hpp
@@ -0,0 +1,49 @@
+#ifndef MATRIX
+#define MATRIX
+
+#include <vector>
+
+class Matrix {
+
+public:
+
+    Matrix(size_t nrow, size_t ncol)
+      : m_nrow(nrow), m_ncol(ncol)
+    {
+        size_t nelement = nrow * ncol;
+        m_buffer.resize(nelement, 0);
+    }
+
+    Matrix(size_t nrow, size_t ncol, std::vector<double> const &vec)
+        : m_nrow(nrow), m_ncol(ncol) {
+        m_buffer = vec;
+    }
+
+    // No bound check.
+    double   operator() (size_t row, size_t col) const { return m_buffer[row*m_ncol + col]; }
+    double & operator() (size_t row, size_t col)       { return m_buffer[row*m_ncol + col]; }
+
+    bool operator == (Matrix const &other) {
+        if (this == &other) return true;
+        if (m_nrow != other.m_nrow || m_ncol != other.m_ncol) return false;
+        if (m_buffer == other.m_buffer) return true;
+        else return false;
+    }
+
+    size_t nrow() const { return m_nrow; }
+    size_t ncol() const { return m_ncol; }
+
+
+    size_t m_nrow;
+    size_t m_ncol;
+    std::vector<double> m_buffer;
+
+};
+
+Matrix multiply_naive(const Matrix &mat1, const Matrix &mat2);
+
+Matrix multiply_tile(Matrix &mat1, Matrix &mat2, int tile_size);
+
+Matrix * multiply_mkl(const Matrix & mat1, const Matrix & mat2);
+
+#endif
